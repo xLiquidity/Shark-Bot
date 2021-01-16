bac0d670c6b4302ce1cecf14752566c9832b808f
Lamont V <lxveal1983@gmail.com>
2021-01-15 21:35:51 -0500

    Update sharkcontract.sol

--- Shark Bot/Shark Bot/sharkcontract.sol
+++ Shark Bot/Shark Bot/sharkcontract.sol
@@ -1,2 +1,28 @@
+pragma solidity >=0.5.0;
 
+contract PriceData {
+    uint storedData; // state variable
+    constructor() public {
+        storedData;
+    }
+
+    // function can be only be called from within contract and contracts that inherit it
+     function readPriceData() internal pure returns(string memory) {
+        uint256 ethPriceKyber; 
+        uint256 daiPriceKyber; 
+        uint256 ethPriceUniswap;
+        uint256 daiPriceUniswap;
+
+        if(ethPriceKyber < daiPriceKyber) {
+            return "Trade";
+        }
+        else if(ethPriceUniswap < daiPriceUniswap) {
+            return "trade";   
+             }
+        else {
+            return "No Trade";
+        }
+
+    }
+}
    
