4b306b902ba19bc8a4e6d08465c47cbdf73babc0
marco mariscal <marco.a.mariscal@gmail.com>
2021-01-17 18:56:10 -0500

    updating helpers with price checking function

--- Shark-Bot/helpers/helpers.js
+++ Shark-Bot/helpers/helpers.js
@@ -1,4 +1,9 @@
 const axios = require("axios");
+const Web3 = require("web3");
+const moment = require("moment-timezone");
+
+// WEB3 CONFIG
+const web3 = new Web3(process.env.RPC_URL);
 
 const ONE_SPLIT_API_URL = `https://api.1inch.exchange/v2.0/`;
 
@@ -22,4 +27,56 @@ function formatTradeReturn(tradeOutput) {
     return tradeOutput.toTokenAmount / 10 ** tradeOutput.toToken.decimals;
 }
 
-module.exports = { getQuoteOutput, formatTradeReturn };
+/***
+ * PRICE CHECKING FUNCTION
+ *
+ */
+async function checkPair(args) {
+    const {
+        inputTokenSymbol,
+        inputTokenAddress,
+        outputTokenSymbol,
+        outputTokenAddress,
+        inputAmount,
+    } = args;
+
+    // Sets the initial determination if a contract should execute or not
+    let executable = false;
+
+    // Calls the 1inch API to provide quote of swapping input token to intermediary token
+    const tradeOne = await getQuoteOutput(
+        inputTokenAddress,
+        outputTokenAddress,
+        inputAmount
+    );
+
+    // Calls the 1inch API to provide quote of swapping intermediary token back to input token
+    const tradeTwo = await getQuoteOutput(
+        outputTokenAddress,
+        inputTokenAddress,
+        tradeOne.toTokenAmount
+    );
+
+    // Checks if the final output is greater than the input amount by equal or greater than 5%
+    if (Number(inputAmount) * 1.05 <= tradeTwo.toTokenAmount) {
+        executable = true;
+    }
+
+    const tradeOneReturn = formatTradeReturn(tradeOne, tradeOne.decimals);
+    const tradeTwoReturn = formatTradeReturn(tradeTwo, tradeTwo.decimals);
+
+    // Creates table of above data plus timestamp
+    console.table([
+        {
+            "Input Token": inputTokenSymbol,
+            "Output Token": outputTokenSymbol,
+            "Input Amount": web3.utils.fromWei(inputAmount, "Ether"),
+            "TradeOne Return": tradeOneReturn,
+            "TradeTwo Return": tradeTwoReturn,
+            "Execute trade": executable,
+            Timestamp: moment().tz("America/Chicago").format(),
+        },
+    ]);
+}
+
+module.exports = { getQuoteOutput, formatTradeReturn, checkPair };
--- Shark-Bot/index.js
+++ Shark-Bot/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const http = require("http");
 const Web3 = require("web3");
 const moment = require("moment-timezone");
-const { getQuoteOutput, formatTradeReturn } = require("./helpers/helpers");
+const { checkPair } = require("./helpers/helpers");
 
 // SERVER CONFIG
 const PORT = process.env.PORT || 5000;
@@ -14,55 +14,6 @@ const server = http
 
 // WEB3 CONFIG
 const web3 = new Web3(process.env.RPC_URL);
-
-// PRICE CHECKING FUNCTION
-async function checkPair(args) {
-    const {
-        inputTokenSymbol,
-        inputTokenAddress,
-        outputTokenSymbol,
-        outputTokenAddress,
-        inputAmount,
-    } = args;
-
-    // Sets the initial determination if a contract should execute or not
-    let executable = false;
-
-    // Calls the 1inch API to provide quote of swapping input token to intermediary token
-    const tradeOne = await getQuoteOutput(
-        inputTokenAddress,
-        outputTokenAddress,
-        inputAmount
-    );
-
-    // Calls the 1inch API to provide quote of swapping intermediary token back to input token
-    const tradeTwo = await getQuoteOutput(
-        outputTokenAddress,
-        inputTokenAddress,
-        tradeOne.toTokenAmount
-    );
-
-    // Checks if the final output is greater than the input amount by equal or greater than 5%
-    if (Number(inputAmount) * 1.05 <= tradeTwo.toTokenAmount) {
-        executable = true;
-    }
-
-    const tradeOneReturn = formatTradeReturn(tradeOne, tradeOne.decimals);
-    const tradeTwoReturn = formatTradeReturn(tradeTwo, tradeTwo.decimals);
-
-    // Creates table of above data plus timestamp
-    console.table([
-        {
-            "Input Token": inputTokenSymbol,
-            "Output Token": outputTokenSymbol,
-            "Input Amount": web3.utils.fromWei(inputAmount, "Ether"),
-            "TradeOne Return": tradeOneReturn,
-            "TradeTwo Return": tradeTwoReturn,
-            "Execute trade": executable,
-            Timestamp: moment().tz("America/Chicago").format(),
-        },
-    ]);
-}
 
 let priceMonitor;
 let monitoringPrice = false;
