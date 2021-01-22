6aec822fa564ba08175b6ce314ad9af2a515d82f
Lamont V <lxveal1983@gmail.com>
2021-01-22 11:12:07 -0500

    Update sharkcontract.sol

--- Shark-Bot/sharkcontract.sol
+++ Shark-Bot/sharkcontract.sol
@@ -1,1 +1,28 @@
+pragma solidity >=0.5.0;
+
+contract TokenSwap {
+    uint storedData; // state variable
+    constructor() public {
+        storedData;
+    }
+
+    // function can be only be called from within contract and contracts that inherit it
+     function getSwapToken() internal pure returns(string memory) {
+        uint256 eth1Inch;
+        uint256 dai1Inch;
+        uint256 usdc1Inch;
+        
+     event (address indexed tokenOwner, address indexed spender, uint tokens);
+     event Transfer(address indexed from, address indexed to, uint tokens);
+     
+     
+        
+        }
+        
+     function getSwapToken(){
+}
+
+     function
+
+
 
